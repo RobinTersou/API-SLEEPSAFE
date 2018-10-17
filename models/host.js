c16c4226bd8c588fc44ce734e1b372e5e26e15ec
diuXd9gfdwco2P <deslifounet@gmail.com>
2018-10-17 17:48:20 +0100

    Update models/host.js

---
+++ models/host.js
@@ -0,0 +1,56 @@
+module.exports = function (sequelize, DataTypes) {
+    const Host = sequelize.define('Host', {
+        id: {
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        nb_bed : {
+          type : DataTypes.DOUBLE,
+          allowNull : false
+        },
+        distance : {
+          type : DataTypes.DOUBLE,
+          allowNull : false
+        },
+        address_number : {
+          type : DataTypes.DOUBLE,
+          allowNull : false
+        },
+        address_name : {
+          type : DataTypes.STRING,
+          allowNull: false
+        },
+        address_zipcode : {
+          type : DataTypes.STRING,
+          allowNull : false
+        },
+        address_city : {
+          type : DataTypes.STRING,
+          allowNull : false
+        },
+        price: {
+            type: DataTypes.DOUBLE,
+            allowNull: false
+        },
+        urlimg: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    }, {
+        paranoid: true,
+        underscored: true,
+        freezeTableName: true
+    });
+    return Host;
+};
+
+// INTERNAL
+
+function _associate(models) {
+
+}
