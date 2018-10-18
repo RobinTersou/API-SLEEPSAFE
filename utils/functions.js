aa4fda76f23453db4c9449769fe5c9f3f68ffbd2
Robin Tersou <tersou.robin@gmail.com>
2018-10-18 17:23:19 +0200

    notifs

---
+++ utils/functions.js
@@ -0,0 +1,26 @@
+const NotificationController = function() { };
+const FCM = require('fcm-push');
+const serviceAccount = require("./fir-storage-sdk");
+
+NotificationController.send = function (user_token, surname, nb_bed) {
+  var serverkey = serviceAccount.serveur_key;
+  var fcm = new FCM(serverkey);
+  var registrationToken = user_token;message = {
+    "to" : registrationToken,
+    "notification" : {
+      "title" : "Demande d'hebergement",
+      "body" : "Bonjour " + surname + ", vous avez une demande d'herbergement pour " + nb_bed + " lits",
+      "icon" : "../iconApp.svg",
+      "click_action" : "kdshvlshl"
+    }
+  };
+  fcm.send(message, function(err,response){
+    if(err) {
+      return 0;
+    } else {
+      return 1;
+    }
+    return 0;
+  });
+};
+module.exports = NotificationController;
